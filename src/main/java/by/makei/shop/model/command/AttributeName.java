4c09a5379a3588ba7aec4865285d6717ee940762
BarmaleyGMN <pavel.makei@gmail.com>
2022-04-15 09:34:42 +0300

    go on

--- src/main/java/by/makei/shop/model/command/AttributeName.java
+++ src/main/java/by/makei/shop/model/command/AttributeName.java
@@ -1,6 +1,8 @@
 package by.makei.shop.model.command;
 
-public final class AttributeName {
+public enum AttributeName {
+    ;
+
     public static final String COMMAND = "command";
     public static final String LOGIN = "login";
     public static final String PASSWORD = "password";
